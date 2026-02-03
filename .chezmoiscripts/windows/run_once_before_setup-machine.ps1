43c4e11863718ac33cf945732e690e95a55daed5
kermage <genealyson.torcende@gmail.com>
2026-02-04 01:24:52 +0800

    update configs

--- .chezmoiscripts/windows/run_once_before_setup-machine.ps1
+++ .chezmoiscripts/windows/run_once_before_setup-machine.ps1
@@ -2,6 +2,7 @@ Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 
 if ( -Not ( Get-Command scoop -ErrorAction SilentlyContinue ) ) {
 	Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+	scoop install git
 	scoop bucket add extras
 	scoop bucket add nerd-fonts
 }
@@ -9,5 +10,3 @@ if ( -Not ( Get-Command scoop -ErrorAction SilentlyContinue ) ) {
 if ( -Not ( Get-Command pnpm -ErrorAction SilentlyContinue ) ) {
 	iwr https://get.pnpm.io/install.ps1 -useb | iex
 }
-
-Add-Content -Path "$HOME\.config\chezmoi\chezmoi.yaml" -Value "interpreters:`n  sh:`n    command: bash"
